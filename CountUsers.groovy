66de60b587d58b7b715fc3ad4cb39c195fe795ab
Jose Armesto <github@armesto.net>
2019-04-26 15:08:56 +0200

    Moved scripts from plugin repository

---
+++ CountUsers.groovy
@@ -0,0 +1,115 @@
+@Grab('org.codehaus.groovy:groovy-json:2.5.6')
+@Grab('org.codehaus.groovy:groovy-dateutil:2.5.6')
+@Grab('info.picocli:picocli:3.9.3')
+@Command(
+        name = "count-user-activity.sh",
+        description = "Count users from /out/aggregated-user-activity.json",
+        showDefaultValues = true
+)
+@picocli.groovy.PicocliScript
+import groovy.json.JsonOutput
+import groovy.json.JsonSlurper
+import groovy.transform.Field
+import groovy.transform.SourceURI
+
+import java.text.SimpleDateFormat
+
+import static picocli.CommandLine.*
+
+Map buildSummary(File reportFile, Date from) {
+
+    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd")
+
+    def report = new JsonSlurper().parseText(reportFile.text)
+
+    def authAccess = report.authAccess.findAll { dateFormat.parse(it.firstDayOfWeek).after(from) }
+    def authAccessFrom = authAccess*.firstDayOfWeek.min() ?: dateFormat.format(from)
+    def authAccessTo = authAccess*.firstDayOfWeek.max() ?: dateFormat.format(new Date())
+    def authAccessUsers = authAccess*.name.unique()
+
+    println "${authAccessUsers.size()} user(s) counted as Authenticated from ${authAccessFrom} to ${authAccessTo}"
+
+    def scmAccess = report.scmAccess.findAll { dateFormat.parse(it.firstDayOfWeek).after(from) }
+    def scmAccessFrom = scmAccess*.firstDayOfWeek.min() ?: dateFormat.format(from)
+    def scmAccessTo = scmAccess*.firstDayOfWeek.max() ?: dateFormat.format(new Date())
+    def scmAccessUsers = scmAccess*.name.unique()
+
+    println "${scmAccessUsers.size()} user(s) counted as SCM contributor from ${scmAccessFrom} to ${scmAccessTo}"
+
+    def totalAccessFrom = [authAccessFrom, scmAccessFrom].min() ?: dateFormat.format(from)
+    def totalAccessTo = [authAccessTo, scmAccessTo].max() ?: dateFormat.format(new Date())
+    def totalUsers = (authAccessUsers + scmAccessUsers).unique()
+
+    println "${totalUsers.size()} user(s) counted as CloudBees Core users from ${totalAccessFrom} to ${totalAccessTo}"
+
+    return [
+            "from"     : totalAccessFrom,
+            "to"       : totalAccessTo,
+            "authUsers": authAccessUsers,
+            "scmUsers" : scmAccessUsers]
+}
+
+String getRelativePath(File base, File path) {
+    return base.toURI().relativize(path.toURI()).getPath();
+}
+
+File reportFile(File workDir) {
+    File reportFile = new File(workDir, "/out/aggregated-user-activity.json")
+    if (reportFile.exists() && !reportFile.isFile()) {
+        println "[ERROR] The path " + getRelativePath(workDir, reportFile) + " isn't a file."
+        System.exit(1)
+    }
+    if (!reportFile.exists()) {
+        println "[ERROR] The report file " + getRelativePath(workDir, reportFile) + " doesn't exist."
+        System.exit(1)
+    }
+    return reportFile
+}
+
+File summaryFile(File workDir) {
+    File summaryFile = new File(workDir, "/out/aggregated-user-activity-summary.json")
+    if (summaryFile.exists() && !summaryFile.isFile()) {
+        println "[ERROR] The path " + getRelativePath(workDir, summaryFile) + " isn't a file."
+        System.exit(2)
+    }
+    if (summaryFile.exists()) {
+        println "[WARN] The summary file " + getRelativePath(workDir, summaryFile) + " is existing and will be overriden."
+    }
+    return summaryFile
+}
+
+
+// The path of the Groovy script
+@SourceURI
+URI sourceUri
+
+// The folder where the Groovy script is
+File workDir = new File(sourceUri).parentFile
+
+println "Working directory: ${workDir}"
+
+// Help CLI option
+@Option(names = ["-h", "--help"], usageHelp = true, description = "Show this help message and exit.")
+@Field boolean helpRequested
+
+// From CLI option
+@Option(names = ["-f", "--from"], description = "Start date to count the number of users. Optional, default is 1 year ago.")
+@Field Date from = new Date().minus(365)
+
+// The summary file we generate
+File summaryFile = summaryFile(workDir)
+
+println "Counting users ..."
+
+summaryFile.withWriter { writer ->
+    writer.write(JsonOutput.prettyPrint(
+            JsonOutput.toJson(
+                    buildSummary(reportFile(workDir), from)
+            )
+    ))
+}
+
+println "Detailed summary available in: " + getRelativePath(workDir, summaryFile)
+
+System.exit(0)
+
---
+++ get-user-activity-monitoring-reports.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+set -euo pipefail
+
+HERE="$( cd -P "$( dirname "${BASH_SOURCE[0]}}" )" && pwd )"
+# shellcheck source=/dev/null
+source "${HERE}/.env"
+
+# VERIFY NEED $OPS_CENTER_URL, JENKINS_USER_ID, JENKINS_API_TOKEN
+if [[ -z "${OPS_CENTER_URL:-""}" ]]; then
+    echo "Need to set environment variable OPS_CENTER_URL (CloudBees Core Ops Center root URL)."
+    exit 1
+fi
+
+if [[ -z "${JENKINS_USER_ID:-""}" ]]; then
+    echo "Need to set environment variable JENKINS_USER_ID."
+    exit 1
+fi
+
+if [[ -z "${JENKINS_API_TOKEN:-""}" ]]; then
+    echo "Need to set environment variable JENKINS_API_TOKEN."
+    exit 1
+fi
+
+# INSTALL JENKINS CLI
+mkdir -p "${HERE}/.jenkins"
+JENKINS_CLI_JAR="${HERE}/.jenkins/jenkins-cli.jar"
+if [[ -f "$JENKINS_CLI_JAR" ]]
+then
+	echo "Using ${JENKINS_CLI_JAR##${HERE}/}."
+else
+	wget -q -O "${JENKINS_CLI_JAR}" "${OPS_CENTER_URL}/jnlpJars/jenkins-cli.jar"
+fi
+
+echo -n "Collecting list of masters..."
+masters=( $(java -jar "${JENKINS_CLI_JAR}" -s "${OPS_CENTER_URL}" list-masters | IFS='\n' jq -cr '.data.masters[] | select(.status == "ONLINE")') )
+echo " done"
+
+mkdir -p "${HERE}/out/reports"
+mkdir -p "${HERE}/out/logs"
+
+# GENERATE REPORT ON EACH MASTER
+echo "Generate user activity report on..."
+for master in "${masters[@]}"; do
+	masterName=$(echo "${master}" | jq -r '.fullName')
+	masterUrl=$(echo "${master}" | jq -r '.url')
+	echo -en " * ${masterName}: "
+	java -jar "${JENKINS_CLI_JAR}" -s "${masterUrl}" user-activity-report > "${HERE}/out/reports/USER-ACTIVITY-MONITORING-${masterName/\//_}-$(date '+%Y%m%d-%H%M%S').json" 2> "${HERE}/out/logs/USER-ACTIVITY-MONITORING-${masterName/\//_}-$(date '+%Y%m%d-%H%M%S')-error.log" && echo "done" || echo "ko"
+done
